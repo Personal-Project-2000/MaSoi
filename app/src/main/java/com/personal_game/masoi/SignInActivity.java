9306a3e73c670a744bcd52a4f2021f8d845871ed
Nguyễn Quang Sỹ <83626285+nguyenquangsy@users.noreply.github.com>
2022-01-13 16:53:13 +0700

    giao diện đăng nhập

--- app/src/main/java/com/personal_game/masoi/SignInActivity.java
+++ app/src/main/java/com/personal_game/masoi/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.personal_game.masoi;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,8 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
