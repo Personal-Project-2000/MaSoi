9c3893e815fbeac1e132c04d25e7a561a90697c6
Nguyễn Quang Sỹ <83626285+nguyenquangsy@users.noreply.github.com>
2022-01-14 17:50:16 +0700

    giao diện thông tin

--- app/src/main/java/com/personal_game/masoi/InfoActivity.java
+++ app/src/main/java/com/personal_game/masoi/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.personal_game.masoi;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        ImageView imgMain = findViewById(R.id.imgMain);
+        imgMain.setImageResource(R.drawable.logo);
     }
 }
